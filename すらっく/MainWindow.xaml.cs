fad9d7ecfdc47a54996cac85ba9bb937785679f2
Es <radwimps.september@gmail.com>
2015-05-22 22:22:29 +0900

    first commit.

---
+++ すらっく/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace すらっく
+{
+  /// <summary>
+  /// MainWindow.xaml の相互作用ロジック
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private Dictionary<char, string> dict = new Dictionary<char, string>();
+
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      dict['あ'] = "a";
+      dict['い'] = "i";
+      dict['う'] = "u";
+      dict['え'] = "e";
+      dict['お'] = "o";
+      dict['か'] = "ka";
+      dict['き'] = "ki";
+      dict['く'] = "ku";
+      dict['け'] = "ke";
+      dict['こ'] = "ko";
+      dict['さ'] = "sa";
+      dict['し'] = "si";
+      dict['す'] = "su";
+      dict['せ'] = "se";
+      dict['そ'] = "so";
+      dict['た'] = "ta";
+      dict['ち'] = "ti";
+      dict['つ'] = "tu";
+      dict['て'] = "te";
+      dict['と'] = "to";
+      dict['な'] = "na";
+      dict['に'] = "ni";
+      dict['ぬ'] = "nu";
+      dict['ね'] = "ne";
+      dict['の'] = "no";
+      dict['は'] = "ha";
+      dict['ひ'] = "hi";
+      dict['ふ'] = "hu";
+      dict['へ'] = "he";
+      dict['ほ'] = "ho";
+      dict['ま'] = "ma";
+      dict['み'] = "mi";
+      dict['む'] = "mu";
+      dict['め'] = "me";
+      dict['も'] = "mo";
+      dict['や'] = "ya";
+      dict['ゆ'] = "yu";
+      dict['よ'] = "yo";
+      dict['ら'] = "ra";
+      dict['り'] = "ri";
+      dict['る'] = "ru";
+      dict['れ'] = "re";
+      dict['ろ'] = "ro";
+      dict['わ'] = "wa";
+      dict['を'] = "wo";
+      dict['ん'] = "nn";
+      dict['が'] = "ga";
+      dict['ぎ'] = "gi";
+      dict['ぐ'] = "gu";
+      dict['げ'] = "ge";
+      dict['ご'] = "go";
+      dict['ざ'] = "za";
+      dict['じ'] = "zi";
+      dict['ず'] = "zu";
+      dict['ぜ'] = "ze";
+      dict['ぞ'] = "zo";
+      dict['だ'] = "da";
+      dict['ぢ'] = "di";
+      dict['づ'] = "du";
+      dict['で'] = "de";
+      dict['ど'] = "do";
+      dict['ば'] = "ba";
+      dict['び'] = "bi";
+      dict['ぶ'] = "bu";
+      dict['べ'] = "be";
+      dict['ぼ'] = "bo";
+      dict['ぱ'] = "pa";
+      dict['ぴ'] = "pi";
+      dict['ぷ'] = "pu";
+      dict['ぺ'] = "pe";
+      dict['ぽ'] = "po";
+      dict['ぁ'] = "xa";
+      dict['ぃ'] = "xi";
+      dict['ぅ'] = "xu";
+      dict['ぇ'] = "xe";
+      dict['ぉ'] = "xo";
+      dict['っ'] = "xtu";
+      dict['ゃ'] = "xya";
+      dict['ゅ'] = "xyu";
+      dict['ょ'] = "xyo";
+      dict['ゎ'] = "xwa";
+      dict['！'] = "bikun";
+      dict['？'] = "hatena";
+      dict['ー'] = "-";
+      dict['\r'] = "\r";
+      dict['\n'] = "\n";
+    }
+
+    private void EncodeButton_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        EncodedText.Text = string.Empty;
+        StringBuilder builder = new StringBuilder();
+        foreach(var c in PreEncodingText.Text)
+        {
+          string buf;
+          if(dict.TryGetValue(c, out buf))
+          {
+            switch(c)
+            {
+              case '\r':
+                break;
+
+              case '\n':
+                builder.Append(Environment.NewLine);
+                break;
+
+              default:
+                builder.Append(string.Format(":c_{0}:", buf));
+                break;
+            }
+          }
+          else
+            throw new Exception("サポートされていない文字が入力されています");
+        }
+        EncodedText.Text = builder.ToString();
+      }
+      catch(Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+        PreEncodingText.Text = string.Empty;
+        EncodedText.Text = string.Empty;
+      }
+    }
+  }
+}
---
+++ どっとじぇねれーた/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace どっとじぇねれーた
+{
+  /// <summary>
+  /// MainWindow.xaml の相互作用ロジック
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private bool isRunning = false;
+    private Bitmap _bmp = null;
+    private BitmapData _img = null;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Image_Drop(object sender, DragEventArgs e)
+    {
+      var paths = e.Data.GetData(DataFormats.FileDrop) as string[];
+      if(paths != null)
+      {
+        try
+        {
+          string path = paths.First();
+          Uri uri = new Uri(path);
+
+          _bmp = new Bitmap(path);
+          if(_bmp.Width * _bmp.Height > 400)
+            throw new NotSupportedException("画像サイズは　縦*横<400px　まで指定可能です");
+          BitmapImage img = new BitmapImage(uri);
+          ConvertImage.Source = img;
+
+          HelpLabel.Opacity = img == null ? 100 : 0;
+          BeginAccess();
+        }
+        catch(NotSupportedException ex)
+        {
+          MessageBox.Show(ex.Message);
+        }
+      }
+    }
+
+    private void Convert_Click(object sender, RoutedEventArgs e)
+    {
+      if(isRunning)
+      {
+        MessageBox.Show("処理実行中です");
+        return;
+      }
+      isRunning = true;
+
+      ResultText.Text = string.Empty;
+
+      StringBuilder builder = new StringBuilder();
+      for(int y = 0; y < _img.Height; ++y)
+      {
+        for(int x = 0; x < _img.Width; ++x)
+          builder.Append(SelectStampName(ConvertPixel(x, y)));
+        builder.Append(Environment.NewLine);
+      }
+
+      ResultText.Text = builder.ToString();
+      EndAccess();
+      isRunning = false;
+    }
+
+    private int ConvertPixel(int x, int y)
+    {
+      var color = GetPixel(x, y);
+      float f = (2 * color.R + 4 * color.G + color.B) / 7;
+      return (int)Math.Floor(f);
+    }
+
+    private string SelectStampName(int f)
+    {
+      int mod = f % 17;
+      int result;
+      if(mod > 8)
+        result = f + 17 - mod;
+      else
+        result = f - mod;
+
+      return string.Format(":b_{0}:", result.ToString().PadLeft(3, '0'));
+    }
+
+    public void BeginAccess()
+    {
+      _img = _bmp.LockBits(new Rectangle(0, 0, _bmp.Width, _bmp.Height),
+          System.Drawing.Imaging.ImageLockMode.ReadWrite,
+          System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+    }
+
+    public void EndAccess()
+    {
+      if(_img != null)
+      {
+        _bmp.UnlockBits(_img);
+        _img = null;
+      }
+    }
+
+    public Color GetPixel(int x, int y)
+    {
+      if(_img == null)
+        return _bmp.GetPixel(x, y);
+
+      IntPtr adr = _img.Scan0;
+      int pos = x * 3 + _img.Stride * y;
+      byte b = System.Runtime.InteropServices.Marshal.ReadByte(adr, pos + 0);
+      byte g = System.Runtime.InteropServices.Marshal.ReadByte(adr, pos + 1);
+      byte r = System.Runtime.InteropServices.Marshal.ReadByte(adr, pos + 2);
+      return Color.FromArgb(r, g, b);
+    }
+  }
+}
